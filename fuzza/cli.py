390ae82e04ed538c1289dcc178ba442fc505b75a
Raphx <rcxng93@hotmail.com>
2017-07-26 16:43:13 +0800

    Add CLI options to specify encodings

--- fuzza/cli.py
+++ fuzza/cli.py
@@ -3,6 +3,26 @@ import click
 from . import __version__
 from . import __prog__
 from .configuration import Configuration
+from .encoder import Encoder
+
+
+def validate_encoder(ctx, param, value):
+    """
+    Validate the encoder input. Input should consist only of the
+    accepted type of encoding. Multiple values are comma-separated.
+    """
+    encoders = value.split(',')
+    for enc in encoders:
+        if enc not in Encoder.ACCEPTED_ENCODING:
+            raise click.BadParameter(
+                'invalid encoder: {encoder}. '
+                '(choose from {choices})'
+                .format(
+                    encoder=value,
+                    choices=', '.join(Encoder.ACCEPTED_ENCODING)
+                )
+            )
+    return encoders
 
 
 @click.group()
@@ -28,12 +48,20 @@ def cli():
 @click.option('--data-path',
               type=str,
               metavar='<path>',
-              prompt='Path to fuzzing data',
+              prompt='Path to fuzz data',
               help='Path containing fuzz data. Support glob patterns.')
 @click.option('--template-path',
               type=str,
-              metavar='<path>',
+              metavar='[path]',
               help='Path containing template files. Support glob patterns.')
+@click.option('--encoder',
+              type=str,
+              metavar=(
+                  '[{enc}[, ...]]'
+                  .format(enc='|'.join(Encoder.ACCEPTED_ENCODING))
+              ),
+              callback=validate_encoder,
+              help='List of encoding to be sequentially applied to fuzz data.')
 def init(**kwargs):
     """
     Create a fuzzer configuration file.
