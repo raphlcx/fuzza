350a851820c3b6f5e99338a1e60cb60bb92778a5
Raphx <rcxng93@hotmail.com>
2017-08-10 15:28:27 +0800

    Add tests for configuration file read and write

--- tests/test_configuration.py
+++ tests/test_configuration.py
@@ -1,10 +1,20 @@
 import unittest
 
+from pathlib import Path
+
 from fuzza.configuration import _get_cfile_path
+from fuzza.configuration import from_file
 from fuzza.configuration import load
+from fuzza.configuration import to_file
 
 
 class TestConfiguration(unittest.TestCase):
+
+    @classmethod
+    def setUpClass(cls):
+        path = Path(__file__) / '..' / '_testdata' / 'configuration'
+        path = str(path.resolve())
+        cls.conf_path = path
 
     def test_configuration_loading(self):
         config = {
@@ -35,4 +45,18 @@ class TestConfiguration(unittest.TestCase):
         self.assertDictEqual(
             conf,
             {'a': 1}
+        )
+
+    def test_configuration_to_and_from_file(self):
+        config = {
+            'a': 1,
+            'b': 'somestring',
+            'c': True
+        }
+        to_file(config, self.conf_path)
+        conf_from_file = from_file(self.conf_path)
+
+        self.assertDictEqual(
+            config,
+            conf_from_file
         )
